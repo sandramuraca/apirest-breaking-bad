72cfc3bd364c55afb6e09d880a8276ab912bf6ec
Sandra Muraca <sandramuraca78@gmail.com>
2022-01-07 01:52:26 -0300

    card individual - sigue sin funcionar

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ Debes hacer un fetch al recurso específico usando el id que te da la api
 Debes hacer una tarjeta de detalles del recurso
 Seguí el código de la clase anterior para guiarte.*/
 
+//**ver paginacion:****** https://breakingbadapi.com/api/characters?limit=10&offset=10 */
 
-//Seleccion elementos del Dom y Variables
+
+//*****Seleccion elementos del Dom y Variables*******
 const UrlBase = "https://www.breakingbadapi.com/api/"
 const endPointCharacters = "https://breakingbadapi.com/api/characters"
 const endPointEpisodios = "https://breakingbadapi.com/api/episodes"
 
 const botonPersonajes = document.querySelector("#boton-personajes")
+const botonEpisodios = document.querySelector("#boton-episodios")
+const contenedorImgNav = document.querySelector(".contenedor-img-nav")
 
 const hero = document.querySelector(".hero")
 const seccionBuscador = document.querySelector(".seccion-buscador")
@@ -21,13 +25,15 @@ const inputBusqueda = document.querySelector("#input-busqueda")
 
 const seccionPersonajes = document.querySelector(".seccion-personajes")
 const cardPersonajes = document.querySelector("#card-personajes")
+
+const tarjetasIndividuales = document.querySelector(".tarjetas-individuales")
 const contenedorTarjetasIndividuales = document.querySelector(".contenedor-tarjetas-individuales")
 
+
 const seccionEpisodios = document.querySelector(".seccion-episodios")
-const botonEpisodios = document.querySelector("#boton-episodios")
 const cardEpisodios = document.querySelector("#card-episodios")
 
-//Get endpoint personajes
+//*******Get endpoint personajes***********
 const traerInfoPersonajes= () =>{
   fetch(endPointCharacters)
   .then((res) =>  res.json())
@@ -38,7 +44,7 @@ const traerInfoPersonajes= () =>{
 
 traerInfoPersonajes()
 
-//Get endpoint episodios
+//**********Get endpoint episodios************
 const traerInfoEpisodios= () =>{
     fetch(endPointEpisodios)
     .then((res) =>  res.json())
@@ -50,7 +56,7 @@ const traerInfoEpisodios= () =>{
 traerInfoEpisodios()
 
 
-//crear tarjetas en html de los personajes
+//*********crear tarjetas en html de los personajes***********
 const crearTarjeta = (data) =>{
 
     const mostrarEnHtml = data.reduce ((acc, elemento) =>{
@@ -67,13 +73,13 @@ const crearTarjeta = (data) =>{
         `
     }, "")
     cardPersonajes.innerHTML = mostrarEnHtml
-    //asignarClicksACards()  //esta comentado por que me tira error
+    clickTarjetaPersonaje()  
 }
 
 traerInfoPersonajes()
 
 
-//crear tarjetas en html de los episodios
+//********crear tarjetas en html de los episodios********
 const crearTarjetaEpisodios = (data) =>{
 
     const mostrarEnHtml = data.reduce ((acc, elemento) =>{
@@ -92,30 +98,35 @@ const crearTarjetaEpisodios = (data) =>{
     cardEpisodios.innerHTML = mostrarEnHtml
     
 }
-
 traerInfoEpisodios()
 
 
 
 //Interacciones con los botones del nav
+
+contenedorImgNav.onclick = () => {
+    hero.classList.remove("ocultar")
+    seccionBuscador.classList.add("ocultar")
+    seccionPersonajes.classList.add("ocultar")
+    seccionEpisodios.classList.add("ocultar")
+}
+
 botonPersonajes.onclick = () => {
-    console.log("click en boton")
     hero.classList.add("ocultar")
     seccionBuscador.classList.remove("ocultar")
     seccionPersonajes.classList.remove("ocultar")
-    
+    seccionEpisodios.classList.add("ocultar") 
 }
 
 botonEpisodios.onclick = () => {
-    console.log("click en boton episodios")
     hero.classList.add("ocultar")
     seccionPersonajes.classList.add("ocultar")
-    seccionEpisodios.classList.toggle("ocultar") 
+    seccionEpisodios.classList.remove("ocultar") 
     seccionBuscador.classList.add("ocultar")
 }
 
 
-//busqueda por personajes
+//*****busqueda por personajes en campo busqueda****
 function busquedaPersonajes(busqueda) {
 
     fetch(`https://breakingbadapi.com/api/characters?name=${busqueda}`)
@@ -132,49 +143,49 @@ formBusqueda.onsubmit = (e) => {
 }
 
 
+/*
+variables
+tarjetasIndividuales  
+contenedorTarjetasIndividuales*/
 
-//Card individual
-// const verCardIndividual = (char_id) =>{
-//     fetch(`https://breakingbadapi.com/api/characters/${char_id}`)
-//     .then((res) => res.json())
-//     .then((data) =>{
-//        console.log(data)
+//*******Card individual*******
+const verCardIndividual = (char_id) =>{
+    fetch(`https://breakingbadapi.com/api/characters/${char_id}`)
+    .then((res) => res.json())
+    .then((data) =>{
+       console.log(data)
 
-//        console.log("ver tarjeta individual")
-//        //ocultar contenerdor tarjetar
-//        cardPersonajes.classList.toggle(".ocultar")
+       console.log("ver tarjeta individual")
 
-//        //mostar contenedor tarjeta individual
-//        seccionPersonajes.classList.toggle(".ocultar") 
-       
-//        //crear tarjeta de personaje individUAL
-//        const crearTarjetaIndiviual = () =>{
-//         `
-//         <div class="tarjeta" data-id="${elemento.char_id}">
-//         <div class="contenedor-imagen">
-//             <img id="imagen-personaje" src="${elemento.img}" alt="">
-//         </div>
-//         <p id="nombre-personaje"> <strong>Nombre:</strong> ${elemento.name}</p>
-//         <p id="nickname-personaje">Nick Name:${elemento.nickname}</p>
-//         <p id="ocupacion-personaje">Ocupación: ${elemento.occupation}</p>
-//         <p id="status-personaje">Status: ${elemento.status}</p>
-//         </div>
-//        `
-//        }
-//        contenedorTarjetasIndividuales.innerHTML = crearTarjetaIndiviual
+       const crearTarjetaIndiviual = () =>{
+        `
+        <div class="tarjeta-individual" data-id="${elemento.char_id}">
+        <div class="contenedor-imagen">
+            <img id="imagen-personaje" src="${elemento.img}" alt="">
+        </div>
+        <p id="nombre-personaje"> <strong>Nombre:</strong> ${elemento.name}</p>
+        <p id="nickname-personaje">Nick Name:${elemento.nickname}</p>
+        <p id="ocupacion-personaje">Ocupación: ${elemento.occupation}</p>
+        <p id="status-personaje">Status: ${elemento.status}</p>
+        </div>
+       `
+       }
+       tarjetasIndividuales.innerHTML = crearTarjetaIndiviual
 
-//     })
-// }
+    })
+}
 
-// const asignarClicksACards = () =>{
-//     const cards = document.querySelectorAll(".tarjeta");
+const clickTarjetaPersonaje = () =>{
+    const cards = document.querySelectorAll(".tarjeta");
     
-//     for (let i = 0; i< cards.length; i++) {
+    for (let i = 0; i< cards.length; i++) {
         
-//         cards[i].onclick = () =>{
-//             const idPersonajes = cards[i].dataset.id;
-//             verCardIndividual(idPersonajes);
-//         }
-//     }
-// } 
+        cards[i].onclick = () =>{
+            const idPersonajes = cards[i].dataset.id;
+            contenedorTarjetasIndividuales.classList.remove("ocultar")
+            cardPersonajes.classList.add("ocultar")
+            verCardIndividual(idPersonajes);
+        }
+    }
+} 
 
