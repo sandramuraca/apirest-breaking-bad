b84329c2dad317a9c352ca83cc7f6c008975ba05
Sandra Muraca <sandramuraca78@gmail.com>
2022-04-25 20:41:07 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 //*****Seleccion elementos del Dom y Variables*******
-<<<<<<< HEAD
-const UrlBase = "https://www.breakingbadapi.com/api/";
 const endPointEpisodios = "https://breakingbadapi.com/api/episodes";
-=======
-// No empieces variables con mayuscula, nunca, salvo que sean componentes de React
-// Declaras UrlBase pero nunca la usás
-const UrlBase = "https://www.breakingbadapi.com/api/"
-// esta variable endPointCharacters nunca se usa
-const endPointCharacters = "https://breakingbadapi.com/api/characters"
-const endPointEpisodios = "https://breakingbadapi.com/api/episodes"
-
-const botonInfo = document.querySelector("#boton-info")
-const seccionInfoGeneral = document.querySelector(".info-general")
->>>>>>> 1558d2978359de647ce9424f787f10bf462e16d0
-
 const botonInfo = document.querySelector("#boton-info");
 const seccionInfoGeneral = document.querySelector(".info-general");
-
 const botonPersonajes = document.querySelector("#boton-personajes");
 const botonEpisodios = document.querySelector("#boton-episodios");
 const contenedorImgNav = document.querySelector(".contenedor-img-nav");
-
 const hero = document.querySelector(".hero");
 const seccionBuscador = document.querySelector(".seccion-buscador");
 const formBusqueda = document.querySelector("#form-busqueda");
 const inputBusqueda = document.querySelector("#input-busqueda");
-
 const botonAnterior = document.querySelector("#anterior");
 const botonSiguiente = document.querySelector("#siguiente");
 const contenedorPaginador = document.querySelector(".contenedor-paginador");
-
 const seccionPersonajes = document.querySelector(".seccion-personajes");
 const cardPersonajes = document.querySelector("#card-personajes");
-
 const tarjetasIndividuales = document.querySelector(".tarjetas-individuales");
+
 const contenedorTarjetasIndividuales = document.querySelector(
   ".contenedor-tarjetas-individuales"
 );
@@ -45,7 +27,6 @@ const cardEpisodios = document.querySelector("#card-episodios");
 const footer = document.querySelector("#footer");
 
 //*******Get endpoint personajes***********
-<<<<<<< HEAD
 let paginaActual = 0;
 const traerInfoPersonajes = () => {
   fetch(
@@ -59,20 +40,6 @@ const traerInfoPersonajes = () => {
       crearTarjeta(data);
     });
 };
-=======
-// deja siempre espacio entre operadores: let paginaActual = 0
-let paginaActual= 0
-const traerInfoPersonajes= () =>{
-    fetch(`https://www.breakingbadapi.com/api/characters?limit=5&offset=${paginaActual * 5}`)
-
-    .then(res => res.json())
-    .then(data => {
-        // no dejes console log
-      console.log(data)
-      crearTarjeta(data)
-    })
-}
->>>>>>> 1558d2978359de647ce9424f787f10bf462e16d0
 
 botonAnterior.onclick = () => {
   paginaActual--;
@@ -80,21 +47,11 @@ botonAnterior.onclick = () => {
 };
 
 botonSiguiente.onclick = () => {
-<<<<<<< HEAD
   paginaActual++;
   traerInfoPersonajes();
 };
 
 traerInfoPersonajes();
-=======
-    paginaActual++
-    traerInfoPersonajes()
-}
-  
-// todas estas funciones que se ejecutan apenas carga la página deberian estar al final de todo, 
-// para que el flujo de ejecución sea más claro
-traerInfoPersonajes()
->>>>>>> 1558d2978359de647ce9424f787f10bf462e16d0
 
 //**********Get endpoint episodios************
 const traerInfoEpisodios = () => {
@@ -108,23 +65,11 @@ const traerInfoEpisodios = () => {
 traerInfoEpisodios();
 
 //*********crear tarjetas en html de los personajes***********
-<<<<<<< HEAD
 const crearTarjeta = (data) => {
   const mostrarEnHtml = data.reduce((acc, elemento) => {
     return (
       acc +
       `
-=======
-const crearTarjeta = (data) =>{
-
-    // Acá podés usar el retorno implícito:
-    // const mostrarEnHtml = data.reduce ((acc, elemento) => acc + 
-
-    const mostrarEnHtml = data.reduce ((acc, elemento) =>{
-
-        return acc + 
-        `
->>>>>>> 1558d2978359de647ce9424f787f10bf462e16d0
         <div class="tarjeta" data-id="${elemento.char_id}">
         <div class="contenedor-imagen">
             <img id="imagen-personaje" src="${elemento.img}" alt="">
@@ -142,20 +87,11 @@ const crearTarjeta = (data) =>{
 traerInfoPersonajes();
 
 //********crear tarjetas en html de los episodios********
-<<<<<<< HEAD
 const crearTarjetaEpisodios = (data) => {
   const mostrarEnHtml = data.reduce((acc, elemento) => {
     return (
       acc +
       `
-=======
-const crearTarjetaEpisodios = (data) =>{
-    // usa retorno implicito aca tambien
-    const mostrarEnHtml = data.reduce ((acc, elemento) =>{
-
-        return acc + 
-        `
->>>>>>> 1558d2978359de647ce9424f787f10bf462e16d0
         <div class="tarjeta-episodio" data-id="${elemento.episode_id}" >
         <p style="text-align: center"><strong>Episodio Nro: </strong>${elemento.episode}</p>
         <p> <strong>Titulo:</strong>  ${elemento.title}</p>
@@ -186,21 +122,11 @@ formBusqueda.onsubmit = (e) => {
 traerInfoPersonajes();
 
 //*******Card individual*******
-<<<<<<< HEAD
 const verCardIndividual = (char_id) => {
   fetch(`https://breakingbadapi.com/api/characters/${char_id}`)
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-=======
-// no uses _ para separar palabras en variables. usa camelCase
-const verCardIndividual = (char_id) =>{
-    fetch(`https://breakingbadapi.com/api/characters/${char_id}`)
-    .then((res) => res.json())
-    .then((data) =>{
-        // no dejes console log
-       console.log(data)
->>>>>>> 1558d2978359de647ce9424f787f10bf462e16d0
 
       const elemento = data[0];
 
